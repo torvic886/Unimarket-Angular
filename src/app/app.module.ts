b980da9d42cf6f8526be5d4c4f404cad33137328
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-26 14:57:53 -0500

    Se crea el componente usuarios en unimarket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent
+    ProductosComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
